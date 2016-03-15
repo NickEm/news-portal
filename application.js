8df8a7c5c9d47d8bf5fd537730a3fa8f51d13618
MykolaMorozov <mykola.morozov@edvantis.com>
2016-03-15 20:15:57 +0200

    Procceed progress.

--- application.js
+++ application.js
@@ -1,5 +1,52 @@
 $(document).ready(function ($) {
 
+    var loadCategories = function () {
+        $.ajax({
+                url: "resources/categories.json",
+                dataType: "json"
+            }
+        ).then(
+            function (data, textStatus, jqXHR) {
+                console.log('News received', data);
+                updateCategories(data);
+            },
+            function (jqXHR, textStatus, errorThrown) {
+                console.error('Error', textStatus);
+                console.error('jqXHR', jqXHR);
+                console.error('errorThrown', errorThrown);
+            }
+        );
+    };
+
+    var updateCategories = function (data) {
+        $('.category_grid').empty();
+        data.forEach(function (item, idx) {
+            var navItem =
+                '<div class="col-md-12 custom_border news_block">' +
+                '<div class="image_container">' +
+                '<div class="custom_border">' +
+                '<img src="'+ item.image +'" class="news_image"/>' +
+                '</div>' +
+                '</div>' +
+                '<div>' +
+                '<div class="pull-right">' +
+                '<span class="glyphicon glyphicon-remove news_delete"></span>' +
+                '<span class="glyphicon glyphicon-pencil news_edit"></span>' +
+                '</div>' +
+                '<div class="news_title">' + item.title + '</div>' +
+                '<div class="brief_description">' + item.brief_description + '</div>' +
+                '</div>' +
+                '<div class="pull-right signature_row">' +
+                '<div class="col-md-4">' + item.author + '</div>' +
+                '<div class="col-md-4">' + item.created_date + '</div>' +
+                '</div>' +
+                ' </div>';
+            $('.category_grid').append(navItem);
+        });
+    };
+
+    loadCategories();
+
     var loadNews = function () {
         console.log('News are loadings');
         $.ajax({
@@ -46,6 +93,7 @@ $(document).ready(function ($) {
             $('.news_grid').append(navItem);
         });
     };
+
 
     loadNews();
 
