bc79911054b02937a6820eac5c5edef0f64dcf37
NickEm <nickinem23@gmail.com>
2016-03-16 23:42:34 +0200

    Added url and category navigations.

--- application.js
+++ application.js
@@ -8,12 +8,9 @@ $(document).ready(function () {
             }
         ).then(
             function (data, textStatus, jqXHR) {
-                console.log('News received', data);
                 updateCategories(data);
             },
             function (jqXHR, textStatus, errorThrown) {
-                console.error('Error', textStatus);
-                console.error('jqXHR', jqXHR);
                 console.error('errorThrown', errorThrown);
             }
         );
@@ -21,36 +18,43 @@ $(document).ready(function () {
 
     var updateCategories = function (data) {
         data.forEach(function (item) {
-            /*var category = '<a class="btn btn-default btn-lg" href="' + item.url + '" role="button">' + item.name +'</a>';*/
-            var category = '<a id="' + item.name + '" class="btn btn-default btn-lg" href="#" >' + item.name + '</a>';
+            var category = '<a id="' + item.name + '" class="btn btn-default btn-lg" href="#' + item.name + '" >' + item.name + '</a>';
             $('#category_grid').append(category);
         });
+        var initialCategory = '#Nature';
+        $(initialCategory).addClass("active");
+        history.pushState(null, '', initialCategory);
     };
 
     loadCategories();
 
-    var loadNews = function () {
-        $.ajax({
-                url: "resources/news.json",
-                dataType: "json",
-                async: false
-            }
-        ).then(
-            function (data, textStatus, jqXHR) {
-                console.log('News received', data);
-                updateNews(data);
-            },
+    var loadNews = function (url) {
+        if(url){
+            $.ajax({
+                    url: url,
+                    dataType: "json",
+                    async: false
+                }
+            ).then(
+                function (data, textStatus, jqXHR) {
+                    updateNews(data);
+                },
 
-            function (jqXHR, textStatus, errorThrown) {
-                console.error('Error', textStatus);
-                console.error('jqXHR', jqXHR);
-                console.error('errorThrown', errorThrown);
-            }
-        );
+                function (jqXHR, textStatus, errorThrown) {
+                    console.error('errorThrown', errorThrown);
+                }
+            );
+        } else {
+            clearNews();
+        }
+    };
+
+    var clearNews = function () {
+        $('#news_grid').empty();
     };
 
     var updateNews = function (data) {
-        $('#news_grid').empty();
+        clearNews();
         data.forEach(function (item, idx) {
             var news =
                 '<div class="col-md-12 custom_border news_block">' +
@@ -64,24 +68,38 @@ $(document).ready(function () {
                 '<span class="glyphicon glyphicon-pencil news_edit"></span>' +
                 '</div>' +
                 '<h3>' + item.title + '</h3>' +
-                '<div class="brief_description">' + item.brief_description + '</div>' +
+                '<div class="description">' + item.description + '</div>' +
+                '<div class="pull-right">' +
+                '<div class="col-md-6">' + item.author + '</div>' +
+                '<div class="col-md-6">' + item.created_date + '</div>' +
                 '</div>' +
-                '<div class="pull-right signature_row">' +
-                '<div class="col-md-4">' + item.author + '</div>' +
-                '<div class="col-md-4">' + item.created_date + '</div>' +
-                '</div>' +
-                ' </div>';
+                '</div>';
             $('#news_grid').append(news);
         });
     };
 
-    loadNews();
+    loadNews("resources/news_nature.json");
+
+    //TODO: Ajax calls should not be sync.
 
     //To highlight menu navigation buttons on click
     $(".btn").each(function () {
         $(this).click(function (){
             $(".btn").removeClass("active");
             $(this).addClass("active");
+            var category = $(this).attr("id");
+            var categoryUrl = "";
+            switch(category) {
+                case "Nature":
+                    categoryUrl = "resources/news_nature.json";
+                    break;
+                case "Science":
+                    categoryUrl = "resources/news_science.json";
+                    break;
+                default:
+                    categoryUrl = "";
+            }
+            loadNews(categoryUrl);
         })
     });
 
