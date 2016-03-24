fca8c4123ddba73d95c6862791e2a7af0cf5f1f6
NickEm <nickinem23@gmail.com>
2016-03-25 00:42:05 +0200

    Added tab.

--- application.js
+++ application.js
@@ -10,8 +10,7 @@ $(document).ready(function () {
         this.video = video;
         this.images = images;
         this.pureHtml = function() {
-            var basicBlock = '<div class="col-md-12 custom_border news_block">' +
-                '<div class="image_container">' +
+            var basicBlock = '<div class="image_container">' +
                 '<div class="custom_border">' +
                 '<img src="'+ this.mainImage +'" class="news_image"/>' +
                 '</div>' +
@@ -27,10 +26,15 @@ $(document).ready(function () {
                 '</div>';
             var titleBlock = '';
             var videoBlock = '';
+            //Actually carousel
+            var imagesBlock = '';
             if(selected) {
                 titleBlock = '<h3>' + this.title + '</h3>';
+                if(images instanceof Array && images.length > 0){
+                    imagesBlock = returnCarouselContent(images);
+                }
                 if(video) {
-                    videoBlock = '<div><div class="video_container">' +
+                    videoBlock = '<div><div class="mt20">' +
                         '<video width="100%" controls>' +
                         '<source src="' + this.video + '" type="video/mp4">' +
                         '</video></div></div>';
@@ -39,13 +43,19 @@ $(document).ready(function () {
                 titleBlock = '<h3>' + '<a id="' + this.newsId + '" href="#" >' + this.title + '</a>' + '</h3>';
             }
             var descriptionBlock = '<div class="description">' + this.description + '</div>';
-            var closureBlock = '<div class="row">' +
+            var closureBlock = '<div class="row mt20">' +
                 '<div class="col-md-3 col-md-offset-6">' + this.author + '</div>' +
                 '<div class="col-md-3">' + this.createdDate + '</div>' +
-                '</div>' +
                 '</div>';
 
-            return basicBlock + titleBlock + descriptionBlock + videoBlock + closureBlock;
+            var newsContent = basicBlock + titleBlock + descriptionBlock + imagesBlock + videoBlock + closureBlock;
+            if(selected){
+                var tabBlock = returnNewsTabs();
+                var tabContent = returnTabsContent(newsContent, '<div>Comments</div>');
+                newsContent = tabBlock + tabContent;
+            }
+
+            return '<div class="col-md-12 custom_border news_block">' + newsContent + '</div>';
         };
     };
 
@@ -86,6 +96,9 @@ $(document).ready(function () {
     var appendNews = function (item, selected) {
         var news = new newsItem(item.news_id, item.title, item.description, item.author, item.created_date, item.main_image, item.video, item.images, selected);
         $('#news_grid').append(news.pureHtml());
+        if(selected){
+            $('.carousel').carousel();
+        }
     };
 
     var addExpandNewsListeners = function () {
@@ -121,7 +134,8 @@ $(document).ready(function () {
         this.categoryId = categoryId;
         this.name = name;
         this.pureHtml = function() {
-            return '<a id="category_' + this.categoryId + '" class="btn btn-default btn-lg btn-menu" href="#" >' + this.name + '</a>';
+            /*return '<a id="category_' + this.categoryId + '" class="btn btn-default btn-lg btn-menu" href="#">' + this.name + '</a>';*/
+            return '<li id="category_' + this.categoryId + '" class="btn-menu"><a href="#">' + this.name + '</a></li>';
         };
     };
 
@@ -150,5 +164,43 @@ $(document).ready(function () {
     loadCategories();
 
     loadNews("resources/news_nature.json");
+
+    var returnCarouselContent = function (images) {
+        var navigationItems = '<li data-target="#image-carousel" data-slide-to="0" class="active"></li>';
+        var carouselItems = '<div class="item active"><img src="' + images[0].image_url + '"></div>';
+        for (var i = 1; i < images.length; i++) {
+            navigationItems += '<li data-target="#image-carousel" data-slide-to="' + i + '"></li>';
+            carouselItems += '<div class="item"><img src="' + images[i].image_url + '"></div>';
+        }
+
+        return '<div id="image-carousel" class="mt20 carousel slide" data-ride="carousel">'+
+        '<ol class="carousel-indicators">'+ navigationItems + '</ol>'+
+
+        '<div class="carousel-inner" role="listbox">' + carouselItems + '</div>'+
+
+        '<a class="left carousel-control" href="#image-carousel" role="button" data-slide="prev">'+
+        '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'+
+        '<span class="sr-only">Previous</span>'+
+        '</a>'+
+        '<a class="right carousel-control" href="#image-carousel" role="button" data-slide="next">'+
+        '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'+
+        '<span class="sr-only">Next</span>'+
+        '</a>'+
+        '</div>';
+    };
+
+    var returnNewsTabs = function () {
+        return '<ul class="nav nav-tabs" role="tablist">' +
+            '<li role="presentation" class="active"><a href="#content" aria-controls="content" role="tab" data-toggle="tab">Content</a></li>' +
+            '<li role="presentation"><a href="#comments" aria-controls="comments" role="tab" data-toggle="tab">Comments</a></li>' +
+            '</ul>';
+    };
+
+    var returnTabsContent = function (contentBlock, commentsBlock) {
+       return '<div class="tab-content">'+
+        '<div role="tabpanel" class="tab-pane active" id="content">' + contentBlock + '</div>'+
+        '<div role="tabpanel" class="tab-pane" id="comments">' + commentsBlock + '</div>'+
+        '</div>'
+    };
 
 });
