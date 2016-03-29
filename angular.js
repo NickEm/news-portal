02a5ddb1dbc3926b6bc1c25014832005466cd870
NickEm <nickinem23@gmail.com>
2016-03-30 00:26:24 +0300

    Added horrible routing configuration.

--- angular.js
+++ angular.js
@@ -1,41 +1,88 @@
-var newsPortalApp = angular.module('newsPortalApp', []);
+var newsPortalApp = angular.module('newsPortalApp', ['ngRoute']);
 
-newsPortalApp.config(['$routeProvider',
+newsPortalApp.config(
     function ($routeProvider) {
         $routeProvider
-            .when('news/nature', {
-                templateUrl: 'news/nature',
-                controller: 'listOfNewsController'})
-            .when('news/science', {
-                templateUrl: 'news/science',
-                controller: 'ShowOrdersController'})
-            .when('news/science', {
-                templateUrl: 'news/science',
-                controller: 'ShowOrdersController'})
+            .when('/news/nature/:newsId', {
+                templateUrl: 'templates/listOfNews.htm',
+                controller: 'particularNewsController'
+            })
+            .when('/news/science', {
+                templateUrl: 'templates/listOfNews.htm',
+                controller: 'listOfScienceNewsController'
+            })
+            .when('/news/nature', {
+                templateUrl: 'templates/listOfNews.htm',
+                controller: 'listOfNatureNewsController'
+            })
             .otherwise({
-                redirectTo: 'news/nature'
-        });
+                redirectTo: '/'
+            });
     }
-]);
+);
 
-
-sampleApp.controller('particularNewsController', function($scope, $routeParams) {
-
-    $scope.order_id = $routeParams.orderId;
-
+newsPortalApp.controller('particularNewsController', function($scope, $routeParams) {
+    debugger;
+    $scope.order_id = $routeParams.newsId;
 });
 
-newsPortalApp.controller('listOfNewsController', function ($scope, newsService) {
+/*newsPortalApp.controller('listOfNewsController', function ($scope, $routeParams, natureNewsService, scienceNewsService) {
+    debugger;
     $scope.listOfNews = [];
-    newsService.getListOfNews().then(function(data) { $scope.listOfNews = data });
+    $scope.$on('$routeChangeSuccess', function() {
+        debugger;
+        switch($routeParams.type) {
+            case 'nature':
+                natureNewsService.getListOfNews().then(function(data) { $scope.listOfNews = data });
+                break;
+            case 'science':
+                scienceNewsService.getListOfNews().then(function(data) { $scope.listOfNews = data });
+                break;
+        }
+    });
+});*/
 
+newsPortalApp.controller('listOfScienceNewsController', function ($scope, $routeParams, scienceNewsService) {
+    debugger;
+    $scope.listOfNews = [];
+    scienceNewsService.getListOfNews().then(function (data) {
+        $scope.listOfNews = data;
+    });
 });
 
-newsPortalApp.service('newsService', function ($http, $q){
+newsPortalApp.controller('listOfNatureNewsController', function ($scope, $routeParams, natureNewsService) {
+    debugger;
+    $scope.listOfNews = [];
+    natureNewsService.getListOfNews().then(function (data) {
+        $scope.listOfNews = data;
+    });
+});
+
+newsPortalApp.service('natureNewsService', function ($http, $q){
     var service = {};
     service.getListOfNews = function(){
         var defer = $q.defer();
         $http.get("http://private-a5cdd5-newsportal.apiary-mock.com/news/nature")
+            .then(
+                function(response){
+                    console.log('News are loaded successfully.');
+                    defer.resolve(response.data);
+                },
+                function(response){
+                    console.log('Error during loading news.');
+                    defer.reject();
+                }
+            );
+        return defer.promise;
+    };
+    return service;
+});
+
+newsPortalApp.service('scienceNewsService', function ($http, $q){
+    var service = {};
+    service.getListOfNews = function(){
+        var defer = $q.defer();
+        $http.get("http://private-a5cdd5-newsportal.apiary-mock.com/news/science")
             .then(
                 function(response){
                     console.log('News are loaded successfully.');
