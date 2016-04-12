fd0261db2eaad1a0d7212694308e1e0d3e072964
MykolaMorozov <mykola.morozov@edvantis.com>
2016-04-12 20:19:53 +0300

    XX-0000 - Started with MongoDb

--- express_server.js
+++ express_server.js
@@ -1,21 +1,31 @@
 // set up ======================================================================
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var port  	 = process.env.PORT || 3003; 				// set the port
+var express = require('express');
+var app = express(); 								// create our app w/ express
+var port = process.env.PORT || 3003; 				// set the port
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var mongojs = require('mongojs');
+var db = mongojs('news', ['news']);
 
 // configuration ===============================================================
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+app.get('/news/:news_type', function (req, res) {
+    console.log('Request for the news of type: ' + req.params.news_type);
+});
+
 // routes ======================================================================
-require('./routes/routes.js')(app);
+app.get('*', function(req, res) {
+    // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
-console.log("App listening on port " + port);
+console.log("Application started on port: " + port);
